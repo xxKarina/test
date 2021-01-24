19cfe3f207a1eee8a90af7ca657809e7335f6794
tushang.yxx <tushang.yxx@alibaba-inc.com>
2021-01-24 15:45:09 +0800

    init

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,307 @@
+// import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography } from 'antd';
+import { useIntl, FormattedMessage } from 'umi';
+import styles from './Welcome.less';
+
+import React, { useRef, useState } from 'react';
+import { PlusOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { Button, Tag, Space, Menu, Dropdown } from 'antd';
+import type { ProColumns, ActionType } from '@ant-design/pro-table';
+import ProTable, { TableDropdown } from '@ant-design/pro-table';
+import request from 'umi-request';
+
+
+
+import { message } from 'antd';
+import ProForm, {
+  ModalForm,
+  ProFormText,
+  ProFormDateRangePicker,
+  ProFormSelect,
+} from '@ant-design/pro-form';
+
+type GithubIssueItem = {
+  url: string;
+  id: number;
+  number: number;
+  title: string;
+  labels: {
+    name: string;
+    color: string;
+  }[];
+  state: string;
+  comments: number;
+  created_at: string;
+  updated_at: string;
+  closed_at?: string;
+};
+const handleCheck = () => {
+
+}
+const waitTime = (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, time);
+  });
+};
+let visible1 = false;
+let setVisible1: any = null;
+
+let selectRow1: any;
+let setSelectRow1: any = null;
+
+const columns: ProColumns<GithubIssueItem>[] = [
+  {
+    dataIndex: 'index',
+    valueType: 'indexBorder',
+    width: 48,
+  },
+  {
+    title: '标题',
+    dataIndex: 'title',
+    copyable: true,
+    ellipsis: true,
+    tip: '标题过长会自动收缩',
+    formItemProps: {
+      rules: [
+        {
+          required: true,
+          message: '此项为必填项',
+        },
+      ],
+    },
+    width: '30%',
+  },
+  {
+    title: '状态',
+    dataIndex: 'state',
+    filters: true,
+    onFilter: true,
+    valueType: 'select',
+    valueEnum: {
+      all: { text: '全部', status: 'Default' },
+      open: {
+        text: '未解决',
+        status: 'Error',
+      },
+      closed: {
+        text: '已解决',
+        status: 'Success',
+        disabled: true,
+      },
+      processing: {
+        text: '解决中',
+        status: 'Processing',
+      },
+    },
+  },
+  {
+    title: '标签',
+    dataIndex: 'labels',
+    search: false,
+    renderFormItem: (_, { defaultRender }) => {
+      return defaultRender(_);
+    },
+    render: (_, record) => (
+      <Space>
+        {record.labels.map(({ name, color }) => (
+          <Tag color={color} key={name}>
+            {name}
+          </Tag>
+        ))}
+      </Space>
+    ),
+  },
+  {
+    title: '创建时间',
+    key: 'showTime',
+    dataIndex: 'created_at',
+    valueType: 'date',
+    hideInSearch: true,
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    valueType: 'dateRange',
+    hideInTable: true,
+    search: {
+      transform: (value) => {
+        return {
+          startTime: value[0],
+          endTime: value[1],
+        };
+      },
+    },
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    render: (text, record, _, action) => {
+      return [
+        <a
+          key="editable"
+          onClick={() => {
+            console.log(record, 898090)
+            setVisible1 && setVisible1(true)
+            let data: any = record;
+            setSelectRow1(data)
+            action.startEditable?.(record.id);
+          }}
+        >
+          编辑
+        </a>,
+        <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
+          查看
+        </a>,
+        <TableDropdown
+          key="actionGroup"
+          onSelect={() => action.reload()}
+          menus={[
+            { key: 'copy', name: '复制' },
+            { key: 'delete', name: '删除' },
+          ]}
+        />,
+      ]
+    },
+  },
+];
+const menu = (
+  <Menu>
+    <Menu.Item key="1">1st item</Menu.Item>
+    <Menu.Item key="2">2nd item</Menu.Item>
+    <Menu.Item key="3">3rd item</Menu.Item>
+  </Menu>
+);
+
+
+export default (): React.ReactNode => {
+  const intl = useIntl();
+
+  const actionRef = useRef<ActionType>();
+
+  const [visible, setVisible] = useState(false);
+  const [selectRow, setSelectRow] = useState();
+  
+  visible1 = visible;
+  setVisible1 = setVisible
+
+
+  selectRow1 = selectRow;
+  setSelectRow1 = setSelectRow
+  return (
+    <PageContainer>
+      <ModalForm<{
+      name: string;
+      company: string;
+    }>
+      title={selectRow1 ? selectRow1.id : "test"}
+      visible={visible}
+      // trigger={
+      //   <Button type="primary">
+      //     <PlusOutlined />
+      //     新建表单
+      //   </Button>
+      // }
+      modalProps={{
+        onCancel: () => console.log('run'),
+      }}
+      onFinish={async (values) => {
+        await waitTime(2000);
+        console.log(values.name);
+        message.success('提交成功');
+        return true;
+      }}
+    >
+      <ProForm.Group>
+        <ProFormText
+          width="md"
+          name="name"
+          label="签约客户名称"
+          tooltip="最长为 24 位"
+          placeholder="请输入名称"
+        />
+        <ProFormText width="md" name="company" label="我方公司名称" placeholder="请输入名称" />
+      </ProForm.Group>
+      <ProForm.Group>
+        <ProFormText width="md" name="contract" label="合同名称" placeholder="请输入名称" />
+        <ProFormDateRangePicker name="contractTime" label="合同生效时间" />
+      </ProForm.Group>
+      <ProForm.Group>
+        <ProFormSelect
+          options={[
+            {
+              value: 'chapter',
+              label: '盖章后生效',
+            },
+          ]}
+          width="xs"
+          name="useMode"
+          label="合同约定生效方式"
+        />
+        <ProFormSelect
+          width="xs"
+          options={[
+            {
+              value: 'time',
+              label: '履行完终止',
+            },
+          ]}
+          name="unusedMode"
+          label="合同约定失效效方式"
+        />
+      </ProForm.Group>
+      <ProFormText width="sm" name="id" label="主合同编号" />
+      <ProFormText name="project" disabled label="项目名称" initialValue="xxxx项目" />
+      <ProFormText width="xs" name="mangerName" disabled label="商务经理" initialValue="启途" />
+    </ModalForm>
+    {/* 7990898 */}
+      <ProTable<GithubIssueItem>
+      columns={columns}
+      actionRef={actionRef}
+      request={async (params = {}) =>
+        request<{
+          data: GithubIssueItem[];
+        }>('https://proapi.azurewebsites.net/github/issues', {
+          params,
+        })
+      }
+      editable={{
+        type: 'multiple',
+      }}
+      rowKey="id"
+      search={{
+        labelWidth: 'auto',
+      }}
+      form={{
+        // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+        syncToUrl: (values, type) => {
+          if (type === 'get') {
+            return {
+              ...values,
+              created_at: [values.startTime, values.endTime],
+            };
+          }
+          return values;
+        },
+      }}
+      pagination={{
+        pageSize: 5,
+      }}
+      dateFormatter="string"
+      headerTitle="高级表格"
+      toolBarRender={() => [
+        <Button key="button" icon={<PlusOutlined />} type="primary">
+          新建
+        </Button>,
+        <Dropdown key="menu" overlay={menu}>
+          <Button>
+            <EllipsisOutlined />
+          </Button>
+        </Dropdown>,
+      ]}
+    />
+    </PageContainer>
+  );
+};
